a86e76e4687453f40048ae1e019051dbfe53a042
Loganfreer <loganu308@gmail.com>
2022-02-01 16:41:10 -0700

    Beginning of Lab 1

---
+++ Labs\ascii/main.py
@@ -0,0 +1,13 @@
+"""	
+	ASCII Art
+ 	Updated By: FIXME (5 points)
+ 	Date: FIXME (5 points)
+	CSCI 110
+ 	This program prints ascii graphics on to the common output       /console/monitor.
+"""
+
+print('    |\\_/|   	**********************    	 (\\_/)  ')
+print("   / @  @ \\  	*           Lab 1    	*       (='.'=) ")
+print('  (>  0 <  )                                  ( \" )_( \" )           ')
+
+#FIXME: finish the rest of the art (80 points)

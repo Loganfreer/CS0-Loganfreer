ed81d80a2a4e156f113c7a0917014d3ef96b7792
Loganfreer <loganu308@gmail.com>
2022-02-03 16:36:54 -0700

    Changes to Lab 1

--- Labs\ascii/main.py
+++ Labs\ascii/main.py
@@ -1,14 +1,14 @@
 """	
 	ASCII Art
- 	Updated By: FIXME (5 points)
- 	Date: FIXME (5 points)
+ 	Updated By: Logan Freerksen 
+ 	Date: 02/03/22 
 	CSCI 110
  	This program prints ascii graphics on to the common output       /console/monitor.
 """
 
-print('    |\\_/|   	************************* 	 (\\_/)  ')
-print("   / @ @ \\  	*       ASCII Lab    	*       (='.'=) ")
-print('  (>  0 < )     *   By: Logan Freerksen *     ( \" )_( \" )           ')
+print('    |\\_/|       ************************* 	 (\\_/)  ')
+print('''   / @ @ \\      *       ASCII Lab    	*       (='.'=) ''')
+print('  (>  0 < )     *   By: Logan Freerksen *       (")_(")           ')
 print('    >>X<<       *       CSCI 110        *       ')
 print('    / O \\       *************************')
 #FIXME: finish the rest of the art (80 points)

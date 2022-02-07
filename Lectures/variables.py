4a86e6732b603318febe588cd99d2a6a6f4dd406
Loganfreer <loganu308@gmail.com>
2022-02-07 16:45:57 -0700

    Testing some math operators

--- Lectures/variables.py
+++ Lectures/variables.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+funky = 2 ** 3
+print(funky)
+print(2 ** 3)
+print(10//3)
+print(10/2)
+#This is some interesting stuff
+print(10%3) 
+# % is  the same as the mod function from intro to advanaced math
+print(1000%24)
+print(1000//24)
+print(1000%24)

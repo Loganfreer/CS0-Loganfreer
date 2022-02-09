8151db0500812e4bb51f323d8dfe9711dcc6c3d7
Loganfreer <loganu308@gmail.com>
2022-02-09 16:45:10 -0700

    Created initial program

--- Lectures/bmi_calculator.py
+++ Lectures/bmi_calculator.py
@@ -11,3 +11,19 @@ Step 2: Calculate BMI using inputed values
 Step 3: Print out BMI
 
 '''
+#Step 1
+weight = input("Please enter your weight in kg: ")
+height = input("Please enter your height in meters: ")
+
+#Step 1a
+weight = float(weight)
+height = float(height)
+
+#Step 2
+bmi = weight/height**2
+
+#Step 3
+print("Your BMI with weight", 
+    weight, "and height", 
+    height, "is", bmi)
+

f125d3812d21ce3a0a27fcab5b7018447b64afb7
Loganfreer <loganu308@gmail.com>
2022-02-09 16:37:23 -0700

    initial BMI calculator

---
+++ Lectures/bmi_calculator.py
@@ -0,0 +1,13 @@
+'''
+Name: Logan Freerksen 
+Date: 02/09/22
+
+This program will calculate the BMI of a person
+BMI = kg/m^2
+
+Step 1: Prompt user to enter weight and height
+Step 1a: Convert values to floats
+Step 2: Calculate BMI using inputed values
+Step 3: Print out BMI
+
+'''

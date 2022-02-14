1ff0cda35f9505186ca322511358c29023d92541
Loganfreer <loganu308@gmail.com>
2022-02-14 16:12:59 -0700

    Starting Hangman Assignment

---
+++ Lectures/stdio.py
@@ -0,0 +1,13 @@
+'''
+Name: Logan Freerksen
+Date: 02/14/22
+
+This program will ask the user for their name and show a few stages 
+of the game Hangman.
+
+Step 1: Prompt user to enter their name, and store the name in a variable
+Step 2: Greet the player with their name
+Step 3: Print to user what the program does
+Step 4: Print out several stages of the game Hangman
+
+'''

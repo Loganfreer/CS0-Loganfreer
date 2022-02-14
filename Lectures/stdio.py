0cc77bfdec3ad97350c56b98ac2d2b7e7ac146ee
Loganfreer <loganu308@gmail.com>
2022-02-14 16:53:31 -0700

    Finished the program

--- Lectures/stdio.py
+++ Lectures/stdio.py
@@ -11,3 +11,89 @@ Step 3: Print to user what the program does
 Step 4: Print out several stages of the game Hangman
 
 '''
+
+#Step 1
+Name = input("Hey there, what's your name? \n")
+#Step 2
+print('Hey', Name,'1')
+#Step 3
+print('''The hangman game under construction, maybe you'll get to play it in a few weeks…
+This is what various stages of the hangman game will look like…
+''')
+#Step 4
+print('Stage 0')
+print('''
+    |__________________ 						
+    |/        	| 
+    | 	        
+    |
+    | 
+    |
+    |
+    |	
+    |
+    | 
+    |				              
+-------------
+''')
+print('Stage 1')
+print('''
+    |__________________ 						
+    |/        ___|___
+    | 	     | X  X |
+    |        | ____ |
+    | 	     |______|
+    |
+    |
+    |
+    |
+    | 	
+    |			              
+-------------
+''')
+print('Stage 2')
+print('''
+    |__________________ 						
+    |/        ___|___
+    | 	     | X  X |
+    |        | ____ |
+    | 	     |______|
+    |           |
+    |           |
+    |           |
+    |           |
+    | 	
+    |			              
+-------------
+''')
+print('Stage 3')
+print('''
+    |__________________ 						
+    |/        ___|___
+    | 	     | X  X |
+    |        | ____ |
+    | 	     |______|
+    |          /|\\
+    |         / | \\
+    |        /  |  \\
+    |           |
+    | 	
+    |			              
+-------------
+''')
+print('Stage 4')
+print('''
+    |__________________ 						
+    |/        ___|___
+    | 	     | X  X |
+    |        | ____ |
+    | 	     |______|
+    |          /|\\
+    |         / | \\
+    |        /  |  \\
+    |           |
+    | 	       / \\
+    |         /   \\             
+-------------
+''')
+print('Thank you for watchoing!')

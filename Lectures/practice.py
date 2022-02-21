3926b0e1b2828e03c784a619de01ff9a8d042f7e
Loganfreer <loganu308@gmail.com>
2022-02-21 16:52:13 -0700

    Lectures 2/21/22

--- Lectures/practice.py
+++ Lectures/practice.py
@@ -36,14 +36,34 @@ Define afunction that takes two numbers
 and returns the product of two numbers
 
 step 1 define function to take two numbers
-step 2 return product of two numbers
-step 3 
+step 2 return product of two numbers from function
+step 3 call function with some parameters
+step 4 print out answer
 '''
 
 #Step 1
 def multiply_nums(num1, num2):
     #Step 2
-    return num1*num2
+    multiply_sum = 0
+    multiply_sum = num1*num2
+    return multiply_sum
 
-#step 3 
-answer = multiply_nums(42, 5)
+print(multiply_nums(42, 5))
+
+#step 3
+def test():
+    assert multiply_nums(2, 6) == 12
+    assert multiply_nums(5, 10) == 50
+
+def main():
+    #step 3
+    test()
+    number1 = float(input("Please enter a number: "))
+    number2 = float(input('Please enter another number: '))
+    answer = multiply_nums(number1,number2)
+    print(f'{number1} * {number2} = {answer}')
+
+main()
+ 
+
+
---
+++ Lectures/rectangle_calc.py
@@ -0,0 +1,11 @@
+'''
+step 1: prompt for two sides
+step 2: calc area
+step 3: calc perimeter
+step 4: print out area and perimeter
+'''
+
+def main():
+    #step1
+    side1 = float(input('Please enter side 1: '))
+    side2 = float(input('Please enter side 2: '))

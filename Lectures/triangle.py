0aadefa9db8733c4616b66df08ace432f5195e9e
Loganfreer <loganu308@gmail.com>
2022-02-22 10:38:00 -0700

    Created function to test if function is a triangle

--- Lectures/triangle.py
+++ Lectures/triangle.py
@@ -8,9 +8,10 @@ perimeter will then be printed out for the user
 
 Step 1: prompt user for their name
 Step 2: Prompt for side1, side2, and side3 of triangle and covert to floats
-Step 3: Calculate area using side1, side2, and side3
-Step 4: Calculate perimeter using side1, side2, and side3
-Step 5: Print out area and perimeter to user
+Step 3: Create function to determine if given side values are a triangle
+Step 4: Calculate area using side1, side2, and side3
+Step 5: Calculate perimeter using side1, side2, and side3
+Step 6: Print out area and perimeter to user
 
 '''
 import math
@@ -18,10 +19,25 @@ import math
 Name = input("Hi there, what's your name? ")
 print('Hey there',Name)
 print("Let's calculate the area and perimeter of a triangle for you!")
+
+def valid_triangle_function(a,b,c):
+    if a+b>=c and b+c>=a and a+c>=b:
+        return True
+    else:
+        return False
+    
+
+
 #Step 2
 side1 = float(input('What is the length of side 1: '))
 side2 = float(input('What is the length of side 2: '))
 side3 = float(input('Finally, what is the length of side 3: '))
+
+if valid_triangle_function(side1, side2, side3):
+    print('Alrighty! we are computing that right noow!')
+else:
+    print("Given inputs do not form a triangle, sorry :'(")
+
 #Step 3
 s = (side1+side2+side3)/2
 area = math.sqrt(s*(s-side1)*(s-side2)*(s-side3))

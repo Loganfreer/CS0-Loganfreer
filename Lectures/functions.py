9bd2999bba72724acfbfb174713da87d9d04a0a5
Loganfreer <loganu308@gmail.com>
2022-02-22 16:22:01 -0700

    Updated repository

---
+++ Lectures/functions.py
@@ -0,0 +1,25 @@
+'''
+Name: Logan Freerksen
+Date:02/22/22
+
+This program will define a bunch of functions related to mathtematical
+operaters and will eventually prompt the user for two numbers and will 
+call fumctions using the users entered parameters
+
+Step 1: Define function that takes in two numebrs and returns the sum
+Step 2: Define function that takes in two numbers and returns the product
+Step 3: Define function that takes in two numbers and returns the quotient
+Step 4: Define function that takes in two numbers and returns the difference
+Step 5: Define a finction that takes in two numbers and returns the 
+        remainder of the quotient
+Step 6: Define function that takes in two numbers and returns the first number
+        to the power of the second
+Step 7: Define a function that takes in a number and returns the square root
+Step 8: Prompt user to enter two numbers
+Step 9: Call all of the functions using the user entered parameters 
+        and print out the results
+Step 10: Write a test function for each function using at least two cases
+Step 11: *Bonus* Define a function that returns the larger of the two numbers 
+'''
+#Step 1
+def add

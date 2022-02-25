b3c1b1e941eb49be069b2d74c7cbe381f0022705
Loganfreer <loganu308@gmail.com>
2022-02-25 16:50:16 -0700

    Made program more efficent

--- Lectures/conver_seconds_to_time.py
+++ Lectures/conver_seconds_to_time.py
@@ -9,21 +9,19 @@ Stepp 2b: Convert remaining seconds to minutes
 Step 3: Print out HH:MM:SS from inputed seconds
 
 '''
-def convert_seconds_to_hours(seconds):
-    hours = seconds / 3600
-    return hours
-    
 
-def convert_seconds_to_minutes(seconds):
-    minutes = seconds // 60
-    return minutes
    
+def convert_seconds_time(seconds):
+    hours = seconds // 3600    
+    minutes = (seconds - (hours * 3600)) // 60
+    rem_seconds = seconds % 60
+    return hours, minutes, rem_seconds
+
 
 def main():
     seconds = int(input("Please enter the number of seconds \n"))
-    hours = convert_seconds_to_hours(seconds)
-    minutes = convert_seconds_to_minutes(seconds - (hours*3600))
-    print(f'The time format of seconds is: {hours}:{minutes}:{seconds%60}')
+    hours, minutes, rem_seconds = convert_seconds_time(seconds)
+    print(f'The time format of seconds is: {hours}:{minutes}:{rem_seconds}')
     pass
 main()
 

808a5a4608fde4368eeaebc66e153df408c9009b
Loganfreer <loganu308@gmail.com>
2022-03-09 16:49:59 -0700

    practice with loops

--- Lectures/loops.py
+++ Lectures/loops.py
@@ -1,8 +1,19 @@
 '''Demonstrate loops'''
 
 def main():
-    n = 13
-    for i in range(2, (n//2) +1):
-        print(i)
+    while(keeprunnin):
+        number = input('Please enter a whole number: ')
+        if(number.isnumeric()):
+            print('You entered a whole number!')
+            keeprunnin = False
+        else:
+            print(f'you must enter a whole number, try again')
+    print(f'your number was {number} ')
 
-main()
+
+'''This program will count the number of digits in a whole number'''
+
+def main():
+    n = 1234
+    pass
+

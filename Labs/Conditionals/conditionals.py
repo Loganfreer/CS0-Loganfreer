c2ec4df168591df2d8bbfe319f2e15e3b42b04e9
Loganfreer <loganu308@gmail.com>
2022-03-10 17:01:56 -0700

    made progress on lab

--- Labs/Conditionals/conditionals.py
+++ Labs/Conditionals/conditionals.py
@@ -1,10 +1,10 @@
 """
 Lab - Conditional Statements
 
-By: FIXME
+By: Logan Freerksen #fixed#
 
 CSCI 110
-Date: FIXME
+Date: 09/Mar/2022   #fixed#
 
 Program prompts user to enter a number and determines whether the entered number is even or odd, positive or negative or zero.
 
@@ -21,7 +21,8 @@ even or odd using the following algorithm:
 	4.1 If the number is greater than zero, return True
 	4.2 Otherwise, return False
 
-5. FIXME1 – write algorithm steps to determine if the number is zero or not using a function.
+5. Defne a function that takes a number as a perameter and deetermines if a number is
+   zero using comparison characters #fixed#
 6. Call the functions using the entered number and display the results with proper descriptions.
 7. Test and validate the program output is correct with several different numbers
 """
@@ -31,16 +32,25 @@ import os
 
 
 def isEven(num):
-    even = False
-    # FIXME2
+    #even = False
+    # FIXME2 #fixed#
     # determine whether the num is even or odd
     # return True for even; False otherwise
-    return even
+    if (num%2 == 0):
+        return True
+    else:
+        return False
 
 
 def isPositive(num):
     # FIXME3 – use your algorithm in step 4 to determin if num is 0 or not
-    return True
+    #fixed#
+    if (num > 0):
+        return True
+    elif (num < 0):
+        return False
+    
+
 
 
 def isZero(num):
@@ -54,6 +64,13 @@ def isZero(num):
 # FXIME5:
 # Define a function that takes in a string name argument
 # function greets the name by printing, e.g.: Hello, John! Welcome onboard...
+def whatsName():
+    name = input('What is your name? ')
+    return name
+
+def greet(name):
+    print(f'Hello {name}, welcome to my program')
+
 
 
 def clearScreen():
@@ -75,16 +92,18 @@ def main():
     if ans != 'y':
         sys.exit()  # exit/end the program
 
-    # FIXME6:
+    # FIXME6: #fixed#
     # Prompt user to enter their name and update the name variable
-    name = "Anonymous"
+
 
     # loop until user wants to quit
     while True:
         # clear the screen for subsequent use
         clearScreen()
         # FIXME7: call function defined in FIXME5 to greet the user
-        print("Enter a whole number, {}:".format(name))
+        user = whatsName()
+        greet(user)
+        print(f"Enter a whole number, {user}:")
         anum = input()
         if not anum.strip('-').isdecimal():
             print('Not a number, enter to continue...')
@@ -102,30 +121,39 @@ def main():
             else:
                 print(anum, "is an odd number!")
 
-            # FIXME8
-            # call isPositive function passing anum and print the result with proper description
+            # FIXME8 #fixed#
+            if isPositive(anum):
+                print(f'The number {anum} is a positive number')
+            elif isPositive:
+                print(f'The number {anum} is negative')
+
 
         answer = input(
             "Would you like to check another number? Enter y or yes; anything else to quit: ")
-        if answer == 'y':
+        if answer == ('y' or 'yes'):
             # FIXME9 - make the progrom continue to run if user entered yes
+            #fixed#
             continue
         else:
-            print('Thanks for using the program, {}! Good bye...'.format(name))
-            break
+                print('Thanks for using the program, {}! Good bye...'.format(user))
+        break
 
 
 def test():
     # test function to test other functions
     # FIXME10
+    assert isEven(42) == True
+    assert isPositive(-9) == False
+    assert isZero(3) == False
     # using assert statment write at least 1 test case for
     # isEven, isPositive, isZero functions
     assert isPositive(99) == True
     print('all test cases passed...')
 
 
-# FIXME10
+# FIXME10 #fixed#
 # call test function
+test()
 
 
 # call main function

d818d4b91d364bbae39b03a81a4d5aad0e009546
Loganfreer <loganu308@gmail.com>
2022-04-11 16:34:11 -0600

    Almost finished assignment

--- Lectures/Assignments/Avion.py
+++ Lectures/Assignments/Avion.py
@@ -8,75 +8,34 @@ which the string has the characters FBI present. If FBI is found in any of the s
 output which strings in which it was found. If FBI is not found the the program will print out, 'He got away'
 '''
 
-def is_first_str(s1):
-    for i in range(len(s1)):
-        if s1[i] == 'F':
-            if s1[i] + s1[i+1] + s1[i+2] == 'FBI':
-                return True
-            else:
-                return False
 
-def is_sec_str(s2):
-    for i in range(len(s2)):
-        if s2[i] == 'F':
-            if s2[i] + s2[i+1] + s2[i+2] == 'FBI':
-                return True
-            else:
-                return False
 
-def is_third_str(s3):
-    for i in range(len(s3)):
-        if s3[i] == 'F':
-            if s3[i] + s3[i+1] + s3[i+2] == 'FBI':
-                return True
-            else: 
-                 return False
 
-def is_fourth_str(s4):
-    for i in range(len(s4)):
-        if s4[i] == 'F':
-            if s4[i] + s4[i+1] + s4[i+2] == 'FBI':
-                return True
-            else:
-                return False
+#Putting given input strings into a iterable list
+def create_list(a,b,c,d,e):
+    inp_uts = [a,b,c,d,e]
+    return inp_uts
 
-def is_fifth_str(s5):
-    for i in range(len(s5)):
-        if s5[i] == 'F':
-            if s5[i] + s5[i+1] + s5[i+2] == 'FBI':
-                return True
-            else:
-                return False
+#Looking for the String FBI in the given strings
+def FBI_test(inp_uts):
+    found_statements = 0
+    for i in range(len(inp_uts)):
+        if 'FBI' in inp_uts[i]:
+            found_statements += 1
+            print(i+1, end = ' ')
+    if found_statements == 0:
+        print('HE GOT AWAY!')
 
-def output(i1, i2, i3, i4, i5):
-    if(i1 == True):
-        print("1")
-    elif(i1 == True and i2 == True):
-        print("1 2")
-    elif(i1 == True and i2 == True and i3 == True):
-        print("1 2 3")
-    elif(i1 == True and i2 == True and i3 == True and i4 == True):
-        print("1 2 3 4")
-    elif(i1 == True and i2 == True and i3 == True and i4 == True and i5 == True):
-        print('1 2 3 4 5')
-    
-    else:
-        print("HE GOT AWAY")
-        
-
+#Main running function  
 def main():
-    a = 'N321-CIAFB3'
-    b = 'F3-FI12I'
-    c = 'F-BI-12'
-    d = 'OVO-JE-CIA'
-    e = 'KRIJUMCAR1'
-    i1 = is_first_str(a)
-    i2 = is_sec_str(b)
-    i3 = is_third_str(c)
-    i4 = is_fourth_str(d)
-    i5  = is_fifth_str(e)
-    output(i1, i2, i3, i4, i5)
+    a = input()
+    b = input()
+    c = input()
+    d = input()
+    e = input()
+    inp_uts = create_list(a,b,c,d,e)
+    FBI_test(inp_uts)
     
-
+#Calling main function
 if __name__ == "__main__":
     main()
--- Lectures/test.py
+++ Lectures/test.py
@@ -1,2 +1,26 @@
-b = 'N-FBI1'
-print(b[2] + b[3])
+a = 'N321-CIAFBI3'
+b = 'F3-FI12I'
+c = 'FBI-12'
+d = 'OVO-JE-CIA'
+e = 'KRIJUMCAR1'
+inp_uts = [a,b,c,d,e]
+for i in range(len(inp_uts)):
+    if isinstance(inp_uts[i], str):
+        for j in range(len(inp_uts[i])):
+            if inp_uts[i][j] == 'F':
+                if inp_uts[i][j] + inp_uts[i][j+1] + inp_uts[i][j+2] == 'FBI':
+                    print(i)
+                elif inp_uts[i][j] + inp_uts[i][j+1] + inp_uts[i][j+2] != 'FBI':
+                    continue
+                else:
+                    print('HE GOT AWAY!')
+'''for i in range(len(inp_uts)):
+    if isinstance(inp_uts[i], str):
+        for j in inp_uts[i]:
+            if inp_uts[i][j] == 'F':
+                if inp_uts[i][j] + inp_uts[i][j+1] + inp_uts[i][j+2] == 'FBI':
+                    print(i)
+                else:
+                    print('HE GOT AWAY!')'''
+
+      

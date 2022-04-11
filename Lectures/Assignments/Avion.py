3d04ee7462180adf01cbef89688a2a208074f853
Loganfreer <loganu308@gmail.com>
2022-04-11 17:29:06 -0600

    finished

--- Lectures/Assignments/Avion.py
+++ Lectures/Assignments/Avion.py
@@ -13,18 +13,19 @@ output which strings in which it was found. If FBI is not found the the program 
 
 #Putting given input strings into a iterable list
 def create_list(a,b,c,d,e):
-    inp_uts = [a,b,c,d,e]
+    inp_uts = [a, b, c, d, e]
     return inp_uts
 
 #Looking for the String FBI in the given strings
 def FBI_test(inp_uts):
-    found_statements = 0
+    b = ""
     for i in range(len(inp_uts)):
         if 'FBI' in inp_uts[i]:
-            found_statements += 1
-            print(i+1, end = ' ')
-    if found_statements == 0:
-        print('HE GOT AWAY!')
+            b += str(i+1) + " "
+    if b == "":
+        return 'HE GOT AWAY!'
+    else:
+        return b
 
 #Main running function  
 def main():
@@ -34,7 +35,8 @@ def main():
     d = input()
     e = input()
     inp_uts = create_list(a,b,c,d,e)
-    FBI_test(inp_uts)
+    check = FBI_test(inp_uts)
+    print(check)
     
 #Calling main function
 if __name__ == "__main__":

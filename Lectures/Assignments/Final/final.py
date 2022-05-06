baf6764467c01543bd2e9a8cec546312e1088eee
Loganfreer <loganu308@gmail.com>
2022-05-06 16:51:22 -0600

    Trying to makek progress

--- Lectures/Assignments/Final/final.py
+++ Lectures/Assignments/Final/final.py
@@ -105,23 +105,33 @@ def game(game_word):
     wrong_guessed = ''
     correct = []
     correct.append(letters)
-    
-    for k in range(len(game_word)):
-        print('_'*k)
 
     while (attempts <= 6):
         guess = input("Guess a letter of the alpabet that you think is in the mystery word: ")
+        for k in range(len(game_word)):
+            print('_ '*k, end= '')
         if guess in game_word:
-            print("Good job!")
+            print("\nGood job!")
             for i in range(len(correct)):
-                if guess in correct[i]:
+                if guess == correct[i]:
+                    print(correct[i])
+            
                     
         else:
             print("oops that didnt seem quite right")
             attempts += 1
             print(stages[attempts])
-            wrong_guessed = wrong_guessed + guess
+            wrong_guessed = wrong_guessed +  '' + guess
             print(f'{wrong_guessed}')
+
+        if attempts == 6:
+            play_again = True
+            while (play_again):
+                response = input("It looks like you ran out of tries, would you like to give it another shot? [y/n] ")
+                if not response.lower()  == 'y' or not response.lower() == 'yes':
+                    break 
+
+               
         
 
 

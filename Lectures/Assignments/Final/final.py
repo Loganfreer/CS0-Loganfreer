a6bdc3635e9d9bd50bf277a70fda4d938a3ca215
Loganfreer <loganu308@gmail.com>
2022-05-08 22:38:58 -0600

    making progress

--- Lectures/Assignments/Final/final.py
+++ Lectures/Assignments/Final/final.py
@@ -5,12 +5,29 @@ Date: 04/28/22
 '''
 import random
 
+def user_name():
+    user = input('Welcome to the Hangman game! What is your name? ')
+    return user
+
+def greet_user(user):
+    print(f"Nice to meet you {user}, your goal is to guess the mystery word in less than 6 tries")
+
 def choose_word(words):
     game_word = random.choice(words)
     return str(game_word)
 
+def guess_check(word_list, guess, display_word):
+    for i in range(len(word_list)):
+        if word_list[i] == guess:
+            display_word[i] = guess
+            return True
+        else:
+            return False
+    
+
+
+
 def game(game_word):
-    letters = list(game_word)
     stages = ['''    |__________________ 						
     |/        	| 
     | 	        
@@ -34,8 +51,8 @@ def game(game_word):
     |
     | 	
     |			              
--------------
-''', '''
+    -------------
+    ''', '''
     |__________________ 						
     |/        ___|___
     | 	     | X  X |
@@ -73,8 +90,8 @@ def game(game_word):
     |           |
     | 	
     |			              
--------------
-''', '''
+    -------------
+    ''', '''
     |__________________ 						
     |/        ___|___
     | 	     | X  X |
@@ -102,34 +119,35 @@ def game(game_word):
     -------------
     ''']
     attempts = 0
-    wrong_guessed = ''
-    correct = []
-    correct.append(letters)
+    word_list = list(game_word)
+    blank_word = []
+    print(f"{word_list}")
+    
+    print(f"{stages[0]}")
 
+    for k in range(len(word_list)):
+        blank_word = "_ "*(k+1)
     while (attempts <= 6):
         guess = input("Guess a letter of the alpabet that you think is in the mystery word: ")
-        for k in range(len(game_word)):
-            print('_ '*k, end= '')
-        if guess in game_word:
-            print("\nGood job!")
-            for i in range(len(correct)):
-                if guess == correct[i]:
-                    print(correct[i])
-            
-                    
+        guess = guess.lower
+        if guess_check(word_list, guess, blank_word):
+            print("Great guess!")
+            print(f"{stages[attempts]}")
+            print(f"{blank_word}")
         else:
-            print("oops that didnt seem quite right")
+            print("That wasn't in there, sorry, try again")
             attempts += 1
-            print(stages[attempts])
-            wrong_guessed = wrong_guessed +  '' + guess
-            print(f'{wrong_guessed}')
+            print({stages[attempts]})
+            print(f"{blank_word}")
+
+
 
         if attempts == 6:
             play_again = True
             while (play_again):
                 response = input("It looks like you ran out of tries, would you like to give it another shot? [y/n] ")
                 if not response.lower()  == 'y' or not response.lower() == 'yes':
-                    break 
+                    play_again = False
 
                
         
@@ -141,6 +159,8 @@ def main():
         data = fin.readlines()
         for line in data:
             words.append(line.strip())
+    user = user_name()
+    greet_user(user)
     game_word = choose_word(words)
     print(game_word)
     game(game_word)
